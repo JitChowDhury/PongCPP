242e911bf5ecbb6a193614f907c8b73743ef90ea
JitChowDhury <jitchowdhury8717@gmail.com>
2025-05-07 20:58:06 +0530

    Add Boundary

--- Header/Gameplay/GameplayManager.h
+++ Header/Gameplay/GameplayManager.h
@@ -1,4 +1,5 @@
 #pragma once
+#include "../../Header/Gameplay/Boundary/Boundary.h"
 #include "../../Header/Gameplay/Ball/Ball.h"
 #include "../../Header/Gameplay/Paddle/Paddle.h"
 #include "../../Header/Event/EventManager.h"
@@ -17,14 +18,14 @@ namespace Gameplay
 
 
 		EventManager* event_manager;
+		Boundary* boundary = new Boundary();
 		Ball* ball = new Ball();
 		Paddle* player1 = new Paddle(player1_position_x, player1_position_y);
 		Paddle* player2 = new Paddle(player2_postion_x, player2_postion_y);
 
-
 		void initialize();
 	public:
-		
+
 		GameplayManager(EventManager* manager);
 		void update();
 		void render(RenderWindow* game_window);
--- Source/Gameplay/GameplayManager.cpp
+++ Source/Gameplay/GameplayManager.cpp
@@ -2,22 +2,19 @@
 
 namespace Gameplay
 {
+
 	GameplayManager::GameplayManager(EventManager* manager)
 	{
 		event_manager = manager;
 	}
-	void GameplayManager::initialize() {
-		ball = new Ball();
-		player1 = new Paddle(player1_position_x, player1_position_y);
-		player2 = new Paddle(player2_postion_x, player2_postion_y);
-
-	}
-
 	void GameplayManager::render(RenderWindow* game_window)
 	{
+		boundary->render(game_window);
 		ball->render(game_window);
 		player1->render(game_window);
 		player2->render(game_window);
+
+		
 	}
 	void GameplayManager::update()
 	{
